5ca9db4806a5ffee35db53e688090ecb09f3e119
Access Keys <Access Keys@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-07 11:36:45 +0000

    AccessPress Twitter Feed - Added Promo side banner and released version 1.1.1
    git-svn-id: https://plugins.svn.wordpress.org/accesspress-twitter-feed/trunk@1129517 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- inc/backend/settings.php
+++ inc/backend/settings.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 $aptf_settings = $this->aptf_settings;
 //$this->print_array($aptf_settings);
 ?>
@@ -13,37 +13,53 @@ $aptf_settings = $this->aptf_settings;
             </ul>
         </div>
         <div class="aptf-board-wrapper">
-            <?php if(isset($_SESSION['aptf_msg'])){?>
-            <div class="aptf-message"><?php echo $_SESSION['aptf_msg'];unset($_SESSION['aptf_msg']);?></div>
+            <?php if (isset($_SESSION['aptf_msg'])) { ?>
+                <div class="aptf-message"><?php
+                    echo $_SESSION['aptf_msg'];
+                    unset($_SESSION['aptf_msg']);
+                    ?></div>
             <?php }
             ?>
-            <form method="post" action="<?php echo admin_url().'admin-post.php';?>">
+            <form method="post" action="<?php echo admin_url() . 'admin-post.php'; ?>">
                 <input type="hidden" name="action" value="aptf_form_action"/>
-                <?php 
-        /**
-         * Settings Panel
-         */
-        include('boards/main-settings.php');
-        
-        /**
-         * How To use Panel
-         */
-        include('boards/how-to-use.php');
-        
-        /**
-         * About Panel
-         */
-        include('boards/about.php');
-        ?>
-        <?php 
-        wp_nonce_field('aptf_action_nonce','aptf_nonce_field');
-        $restore_nonce = wp_create_nonce('aptf-restore-nonce');
-        ?>
-        <input type="submit" name="aptf_settings_submit" value="<?php _e('Save Settings',APTF_TD);?>" class="button button-primary"/>
-        <a href="<?php echo admin_url().'admin-post.php?action=aptf_restore_settings&_wpnonce='.$restore_nonce;?>" onclick="return confirm('<?php _e('Are you sure you want to restore default settings?',APTF_TD)?>');"><input type="button" value="<?php _e('Restore Default Settings',APTF_TD);?>" class="button button-primary"/></a>
-        <a href="<?php echo admin_url().'admin-post.php?action=aptf_delete_cache';?>" onclick="return confirm('<?php _e('Are you sure you want to delete cache?',APTF_TD)?>');"><input type="button" value="<?php _e('Delete Cache',APTF_TD);?>" class="button button-primary"/></a>
-    </form>
-</div>
-</div>
+                <?php
+                /**
+                 * Settings Panel
+                 */
+                include('boards/main-settings.php');
+
+                /**
+                 * How To use Panel
+                 */
+                include('boards/how-to-use.php');
+
+                /**
+                 * About Panel
+                 */
+                include('boards/about.php');
+                ?>
+                <?php
+                wp_nonce_field('aptf_action_nonce', 'aptf_nonce_field');
+                $restore_nonce = wp_create_nonce('aptf-restore-nonce');
+                ?>
+                <input type="submit" name="aptf_settings_submit" value="<?php _e('Save Settings', APTF_TD); ?>" class="button button-primary"/>
+                <a href="<?php echo admin_url() . 'admin-post.php?action=aptf_restore_settings&_wpnonce=' . $restore_nonce; ?>" onclick="return confirm('<?php _e('Are you sure you want to restore default settings?', APTF_TD) ?>');"><input type="button" value="<?php _e('Restore Default Settings', APTF_TD); ?>" class="button button-primary"/></a>
+                <a href="<?php echo admin_url() . 'admin-post.php?action=aptf_delete_cache'; ?>" onclick="return confirm('<?php _e('Are you sure you want to delete cache?', APTF_TD) ?>');"><input type="button" value="<?php _e('Delete Cache', APTF_TD); ?>" class="button button-primary"/></a>
+            </form>
+        </div>
+    </div>
+    <div class="aptf-promo">
+        <a href="http://codecanyon.net/item/accesspress-twitter-feed-pro/11029697?ref=AccessKeys" target="_blank"><img src="<?php echo APTF_IMAGE_DIR . '/upgrade-1.jpg' ?>"/></a>
+        <div class="aptf-promo-actions">
+            <a href="http://accesspressthemes.com/demo/wordpress-plugins/accesspress-twitter-feed-pro/" title="Demo" target="_blank"><input type="button" class="aptf-demo-btn" value="Demo"/></a>
+            <a href="http://codecanyon.net/item/accesspress-twitter-feed-pro/11029697?ref=AccessKeys" title="Upgrade" target="_blank"><input type="button" class="aptf-upgrade-btn" value="Upgrade"/></a>
+        </div>
+
+        <a href="http://codecanyon.net/item/accesspress-twitter-feed-pro/11029697?ref=AccessKeys" target="_blank"><img src="<?php echo APTF_IMAGE_DIR . '/upgrade-2.jpg' ?>"/></a>
+        <div class="aptf-promo-actions">
+            <a href="http://accesspressthemes.com/demo/wordpress-plugins/accesspress-twitter-feed-pro/" title="Demo" target="_blank"><input type="button" class="aptf-demo-btn" value="Demo"/></a>
+            <a href="http://codecanyon.net/item/accesspress-twitter-feed-pro/11029697?ref=AccessKeys" title="Upgrade" target="_blank"><input type="button" class="aptf-upgrade-btn" value="Upgrade"/></a>
+        </div>
+    </div>
 </div>
 
--- readme.txt
+++ readme.txt
@@ -4,7 +4,7 @@ Tags: twitter, twitter feeds, twitter slider, twitter feeds slider, twitter plug
 Donate link: http://accesspressthemes.com/donation/
 Requires at least: 3.8
 Tested up to: 4.1
-Stable tag: 1.1.0
+Stable tag: 1.1.1
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
@@ -83,6 +83,9 @@ Once you install the plugin , you can check some general documentation about how
 
 
 == Changelog ==
+= 1.1.1 = 
+* Added Promo Banner in sidebar
+
 = 1.1.0 = 
 * API updated
 * Small bug for time elasped date format fixed
